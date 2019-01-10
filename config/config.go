c175f79b7a7206ece5e1b1dcb161ed9c53c72da8
Robert Johnson <robj@yelp.com>
2019-01-10 08:41:59 -0800

    refactor config package
    creates an interface for a ConfigReader that provides a single function
    for reading config into a struct.

--- config/config.go
+++ config/config.go
@@ -1,13 +1,37 @@
 package config
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"os"
+	"path"
 )
 
-func ReadSystemPaaSTAConfig(f string) *os.File {
-	r, err := os.Open(f)
+type ConfigReader interface {
+	Read(interface{}) error
+}
+
+type SystemPaaSTAConfigFileReader struct {
+	Basedir  string
+	Filename string
+}
+
+func ParseContent(r io.Reader, t interface{}) error {
+	buf, err := ioutil.ReadAll(r)
+	err = json.Unmarshal(buf, t)
+	return err
+}
+
+func (c SystemPaaSTAConfigFileReader) FileNameForConfig() string {
+	return path.Join(c.Basedir, c.Filename)
+}
+
+func (c SystemPaaSTAConfigFileReader) Read(t interface{}) error {
+	r, err := os.Open(c.FileNameForConfig())
+	defer r.Close()
 	if err != nil {
 		panic(err)
 	}
-	return r
+	return ParseContent(r, t)
 }
