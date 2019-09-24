45ec937b149ed45bb572acbaa8fd8f8d95dbc84a
Bronek Kozicki <bronek@yelp.com>
2019-09-24 13:21:43 +0100

    Add DeepCopy support to PaastaContainerSpec

--- containerspec/containerspec.go
+++ containerspec/containerspec.go
@@ -76,3 +76,27 @@ func (spec *PaastaContainerSpec) GetContainerResources() (*corev1.ResourceRequir
 		},
 	}, nil
 }
+
+func (in *PaastaContainerSpec) DeepCopyInto(out *PaastaContainerSpec) {
+    *out = *in
+    if in.CPU != nil {
+        in, out := &in.CPU, &out.CPU
+        *out = new(KubeResourceQuantity)
+        **out = **in
+    }
+    if in.Memory != nil {
+        in, out := &in.Memory, &out.Memory
+        *out = new(KubeResourceQuantity)
+        **out = **in
+    }
+    return
+}
+
+func (in *PaastaContainerSpec) DeepCopy() *PaastaContainerSpec {
+    if in == nil {
+        return nil
+    }
+    out := new(PaastaContainerSpec)
+    in.DeepCopyInto(out)
+    return out
+}
