0dfab1fa430bb5ff1319e4f16adb60dac43636a6
Robert Johnson <rob@rob-johnson.me>
2019-10-17 15:30:24 +0100

    start work on provider for acceptance testing
    this function will be the place we can hide implementation details for
    creating a k8s cluster on demand and providing a client for accessing
    it.
    first implementation relies on the Makefile currently in place for
    operators

---
+++ pkg/acceptance/acceptance.go
@@ -0,0 +1,71 @@
+package acceptance
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/subosito/gotenv"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func buildEnv(makefile string) error {
+	cmd := exec.Command("make", "local-env", "-f", makefile)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	log.Printf("%q", out.String())
+	log.Printf("%q", stderr.String())
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	env, err := gotenv.StrictParse(&out)
+	if err != nil {
+		return err
+	}
+
+	for key, val := range env {
+		fmt.Println(fmt.Sprintf("%s: %s", key, val))
+		if _, present := os.LookupEnv(key); !present {
+			os.Setenv(key, val)
+		}
+	}
+	return nil
+}
+
+func KubernetesCluster(makefile string) (kubernetes.ClientSet, error) {
+	err := buildEnv(makefile)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	cmd := exec.Command("make", "local-cluster", "-f", makefile)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	log.Printf("%q", out.String())
+	log.Printf("%q", stderr.String())
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	configOverrides := &clientcmd.ConfigOverrides{}
+	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+	config, err := kubeConfig.ClientConfig()
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	return clientset, nil
+}
