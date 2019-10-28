91c424c8ed5cb666ca3ec22519d9d2fb4cd9fdbd
Bronek Kozicki <bronek@yelp.com>
2019-10-28 12:09:14 +0000

    Add ephemeral disk to PaastaContainerSpec

--- pkg/containerspec/containerspec.go
+++ pkg/containerspec/containerspec.go
@@ -12,6 +12,7 @@ import (
 const (
 	defaultCPU    = KubeResourceQuantity("0.1")
 	defaultMemory = KubeResourceQuantity("512")
+	defaultDisk   = KubeResourceQuantity("1024")
 )
 
 // KubeResourceQuantity : Resource quantity for Kubernetes (e.g.; CPU, mem, disk)
@@ -35,6 +36,7 @@ func (n KubeResourceQuantity) MarshalJSON() ([]byte, error) {
 type PaastaContainerSpec struct {
 	CPU    *KubeResourceQuantity `json:"cpus"`
 	Memory *KubeResourceQuantity `json:"mem"`
+	Disk   *KubeResourceQuantity `json:"disk"`
 }
 
 // GetContainerResources : get resource requirements based on the container spec
@@ -65,14 +67,31 @@ func (spec *PaastaContainerSpec) GetContainerResources() (*corev1.ResourceRequir
 		return nil, fmt.Errorf("error while parsing memory '%s': %s", memory, err)
 	}
 
+	var disk KubeResourceQuantity
+	if spec.Disk != nil {
+		disk = *spec.Disk
+	} else {
+		disk = defaultDisk
+	}
+	if _, err := strconv.Atoi(string(disk)); err == nil {
+		// value looks like a number, let's treat it as MB according to PaaSTA default
+		disk = disk + "Mi"
+	}
+	diskQuantity, err := resource.ParseQuantity(string(disk))
+	if err != nil {
+		return nil, fmt.Errorf("error while parsing disk '%s': %s", disk, err)
+	}
+
 	return &corev1.ResourceRequirements{
 		Requests: corev1.ResourceList{
-			corev1.ResourceCPU:    cpuQuantity,
-			corev1.ResourceMemory: memoryQuantity,
+			corev1.ResourceCPU:              cpuQuantity,
+			corev1.ResourceMemory:           memoryQuantity,
+			corev1.ResourceEphemeralStorage: diskQuantity,
 		},
 		Limits: corev1.ResourceList{
-			corev1.ResourceCPU:    cpuQuantity,
-			corev1.ResourceMemory: memoryQuantity,
+			corev1.ResourceCPU:              cpuQuantity,
+			corev1.ResourceMemory:           memoryQuantity,
+			corev1.ResourceEphemeralStorage: diskQuantity,
 		},
 	}, nil
 }
@@ -86,6 +105,11 @@ func (in *PaastaContainerSpec) DeepCopyInto(out *PaastaContainerSpec) {
 	}
 	if in.Memory != nil {
 		in, out := &in.Memory, &out.Memory
+		*out = new(KubeResourceQuantity)
+		**out = **in
+	}
+	if in.Disk != nil {
+		in, out := &in.Disk, &out.Disk
 		*out = new(KubeResourceQuantity)
 		**out = **in
 	}
--- pkg/containerspec/containerspec_test.go
+++ pkg/containerspec/containerspec_test.go
@@ -9,7 +9,12 @@ import (
 func TestUnmarshal(t *testing.T) {
 	cpu := KubeResourceQuantity("0.2")
 	mem := KubeResourceQuantity("1024")
-	in := fmt.Sprintf(`{"cpus":"%s","mem":"%s"}`, string(cpu), string(mem))
+	disk := KubeResourceQuantity("2048")
+	in := fmt.Sprintf(`{"cpus":"%s","mem":"%s","disk":"%s"}`,
+		string(cpu),
+		string(mem),
+		string(disk),
+	)
 	var spec PaastaContainerSpec
 	if err := json.Unmarshal([]byte(in), &spec); err != nil {
 		t.Errorf("Failed to unmarshal: %s", err)
@@ -20,10 +25,13 @@ func TestUnmarshal(t *testing.T) {
 	if *spec.Memory != mem {
 		t.Errorf("%s != %s", *spec.Memory, mem)
 	}
+	if *spec.Disk != disk {
+		t.Errorf("%s != %s", *spec.Disk, disk)
+	}
 }
 
 func TestUnmarshalNull(t *testing.T) {
-	in := `{"cpus":null,"mem":null}`
+	in := `{"cpus":null,"mem":null,"disk":null}`
 	var spec PaastaContainerSpec
 	if err := json.Unmarshal([]byte(in), &spec); err != nil {
 		t.Errorf("Failed to unmarshal: %s", err)
@@ -33,6 +41,9 @@ func TestUnmarshalNull(t *testing.T) {
 	}
 	if spec.Memory != nil {
 		t.Errorf("%s != nil", *spec.Memory)
+	}
+	if spec.Disk != nil {
+		t.Errorf("%s != nil", *spec.Disk)
 	}
 }
 
@@ -55,33 +66,40 @@ func checkDeepCopy(t *testing.T, input string) {
 func TestEmptyDeepCopy(t *testing.T) {
 	checkDeepCopy(
 		t,
-		`{"cpus":null,"mem":null}`,
+		`{"cpus":null,"mem":null,"disk":null}`,
 	)
 }
 
 func TestOnlyCPUDeepCopy(t *testing.T) {
 	checkDeepCopy(
 		t,
-		`{"cpus":"0.5","mem":null}`,
+		`{"cpus":"0.5","mem":null,"disk":null}`,
 	)
 }
 
 func TestOnlyMemDeepCopy(t *testing.T) {
 	checkDeepCopy(
 		t,
-		`{"cpus":null,"mem":"2048"}`,
+		`{"cpus":null,"mem":"2048","disk":null}`,
 	)
 }
 
-func TestBothMemCPUDeepCopy(t *testing.T) {
+func TestOnlyDiskDeepCopy(t *testing.T) {
 	checkDeepCopy(
 		t,
-		`{"cpus":"0.25","mem":"2048"}`,
+		`{"cpus":null,"mem":null,"disk":"10240"}`,
+	)
+}
+
+func TestAllDeepCopy(t *testing.T) {
+	checkDeepCopy(
+		t,
+		`{"cpus":"0.25","mem":"2048","disk":"10240"}`,
 	)
 }
 
 func TestJSONRoundTrip(t *testing.T) {
-	in := `{"cpus":"0.2","mem":"1024"}`
+	in := `{"cpus":"0.2","mem":"1024","disk":"4096"}`
 	var spec PaastaContainerSpec
 	if err := json.Unmarshal([]byte(in), &spec); err != nil {
 		t.Errorf("Failed to unmarshal: %s", err)
@@ -118,7 +136,7 @@ func TestEmptyResources(t *testing.T) {
 	checkEqualResources(
 		t,
 		"{}",
-		`{"limits":{"cpu":"100m","memory":"512M"},"requests":{"cpu":"100m","memory":"512M"}}`,
+		`{"limits":{"cpu":"100m","ephemeral-storage":"1Gi","memory":"512M"},"requests":{"cpu":"100m","ephemeral-storage":"1Gi","memory":"512M"}}`,
 	)
 }
 
@@ -126,7 +144,7 @@ func TestOnlyCPUResources(t *testing.T) {
 	checkEqualResources(
 		t,
 		`{"cpus":"0.5"}`,
-		`{"limits":{"cpu":"500m","memory":"512M"},"requests":{"cpu":"500m","memory":"512M"}}`,
+		`{"limits":{"cpu":"500m","ephemeral-storage":"1Gi","memory":"512M"},"requests":{"cpu":"500m","ephemeral-storage":"1Gi","memory":"512M"}}`,
 	)
 }
 
@@ -134,7 +152,15 @@ func TestOnlyMemResources(t *testing.T) {
 	checkEqualResources(
 		t,
 		`{"mem":"1024"}`,
-		`{"limits":{"cpu":"100m","memory":"1024M"},"requests":{"cpu":"100m","memory":"1024M"}}`,
+		`{"limits":{"cpu":"100m","ephemeral-storage":"1Gi","memory":"1024M"},"requests":{"cpu":"100m","ephemeral-storage":"1Gi","memory":"1024M"}}`,
+	)
+}
+
+func TestOnlyDiskResources(t *testing.T) {
+	checkEqualResources(
+		t,
+		`{"disk":"2000"}`,
+		`{"limits":{"cpu":"100m","ephemeral-storage":"2000Mi","memory":"512M"},"requests":{"cpu":"100m","ephemeral-storage":"2000Mi","memory":"512M"}}`,
 	)
 }
 
@@ -142,6 +168,14 @@ func TestBothMemCPUResources(t *testing.T) {
 	checkEqualResources(
 		t,
 		`{"cpus":"0.2","mem":"1024"}`,
-		`{"limits":{"cpu":"200m","memory":"1024M"},"requests":{"cpu":"200m","memory":"1024M"}}`,
+		`{"limits":{"cpu":"200m","ephemeral-storage":"1Gi","memory":"1024M"},"requests":{"cpu":"200m","ephemeral-storage":"1Gi","memory":"1024M"}}`,
+	)
+}
+
+func TestAllResources(t *testing.T) {
+	checkEqualResources(
+		t,
+		`{"cpus":"0.2","mem":"1024","disk":"10Gi"}`,
+		`{"limits":{"cpu":"200m","ephemeral-storage":"10Gi","memory":"1024M"},"requests":{"cpu":"200m","ephemeral-storage":"10Gi","memory":"1024M"}}`,
 	)
 }
