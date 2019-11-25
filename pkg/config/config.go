92d64e64492ba6a60150b46367692a40076dfcdc
Maksym Melnychok <maksym@yelp.com>
2019-11-25 18:25:13 +0100

    implement config stores with support for key hints

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,39 +2,146 @@ package config
 
 import (
 	"encoding/json"
-	"io"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"path"
+	"sync"
 )
 
-type ConfigReader interface {
-	Read(interface{}) error
+// Store holds config data
+type Store struct {
+	// If you care about sanity, never write here, just read
+	data  map[string]interface{}
+	dir   string
+	hints map[string]string
+	sync.Mutex
+
+	listFiles  func(string) ([]string, error)
+	parseFile  func(string, interface{}) error
+	fileExists func(string) (bool, error)
 }
 
-type ConfigFileReader struct {
-	Basedir  string
-	Filename string
-}
-
-func ParseContent(reader io.Reader, content interface{}) error {
-	buf, err := ioutil.ReadAll(reader)
+func listFiles(dirname string) ([]string, error) {
+	files, err := ioutil.ReadDir(dirname)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	err = json.Unmarshal(buf, content)
-	return err
+
+	ret := make([]string, len(files))
+	for idx, file := range files {
+		ret[idx] = file.Name()
+	}
+
+	return ret, nil
 }
 
-func (configReader ConfigFileReader) FileNameForConfig() string {
-	return path.Join(configReader.Basedir, configReader.Filename)
-}
-
-func (configReader ConfigFileReader) Read(content interface{}) error {
-	reader, err := os.Open(configReader.FileNameForConfig())
+func parseFile(path string, value interface{}) error {
+	reader, err := os.Open(path)
 	defer reader.Close()
 	if err != nil {
 		return err
 	}
-	return ParseContent(reader, content)
+
+	return json.NewDecoder(reader).Decode(value)
+}
+
+func fileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+// NewStore creates a new config store
+// `dir` is a directory from where data will be loaded
+// `hints` is a dictionary to find which file to load for
+// a given key
+func NewStore(dir string, hints map[string]string) *Store {
+	if hints == nil {
+		hints = map[string]string{}
+	}
+	return &Store{
+		dir:        dir,
+		hints:      hints,
+		listFiles:  listFiles,
+		parseFile:  parseFile,
+		fileExists: fileExists,
+	}
+}
+
+// Decode `path` contents using `json`, copy `s.data` into
+// a new map, merge loaded data into the copy and swap `s.data`
+func (s *Store) loadPath(path string) error {
+	value := map[string]interface{}{}
+	err := s.parseFile(path, value)
+	if err != nil {
+		return err
+	}
+
+	s.Lock()
+	defer s.Unlock()
+	for key, val := range value {
+		s.data[key] = val
+	}
+
+	return nil
+}
+
+// Walk `s.dir` and `s.loadPath` all the files
+func (s *Store) loadAll() error {
+	files, err := s.listFiles(s.dir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		err := s.loadPath(path.Join(s.dir, file))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Look for `domain`.json file, if not found try loading all files and
+// print a warning about hints
+func (s *Store) load(domain string) error {
+	path := path.Join(s.dir, fmt.Sprintf("%s.json", domain))
+	exists, err := s.fileExists(path)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		log.Printf(
+			"WARN: loading all configs, consider adding some hints" +
+				fmt.Sprintf("for %s in %s", domain, s.dir),
+		)
+		return s.loadAll()
+	}
+	return s.loadPath(path)
+}
+
+// Get returns value for given `key`. If not found in `s.data`, call
+// `s.load` function with `domain` from `s.hints` or `key` itself.
+func (s *Store) Get(key string) (interface{}, bool) {
+	if val, ok := s.data[key]; ok {
+		return val, ok
+	}
+
+	var domain string
+	if dom, ok := s.hints[key]; ok {
+		domain = dom
+	} else {
+		domain = key
+	}
+	s.load(domain)
+
+	val, ok := s.data[key]
+	return val, ok
 }
--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,54 +1,166 @@
 package config
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"reflect"
+	"fmt"
 	"testing"
 )
 
-type FakeConfig struct {
-	Foo string
-}
-
-type fakereader struct {
-	reader io.Reader
-	data   *FakeConfig
-}
-
-func (fakereader fakereader) Read(bytes []byte) (n int, err error) {
-	return fakereader.reader.Read(bytes)
-}
-
-func fakeDataReader(config *FakeConfig) fakereader {
-	content, _ := json.Marshal(*config)
-	return fakereader{
-		data:   config,
-		reader: bytes.NewReader(content),
+func errorIf(test *testing.T, pred bool, format string, args ...interface{}) {
+	if pred {
+		test.Fatalf(format, args...)
 	}
 }
 
-func TestParseContent(test *testing.T) {
-	fakeData := &FakeConfig{}
-	reader := fakeDataReader(fakeData)
-	err := ParseContent(reader, fakeData)
-	if err != nil {
-		test.Errorf("failed to decode content")
+func errorUnexpected(test *testing.T, expected, actual interface{}) {
+	errorIf(test, expected != actual, "expected %+v, actual %+v", expected, actual)
+}
+
+func TestStore_loadPath(test *testing.T) {
+	key := "test key"
+	s := &Store{
+		data: map[string]interface{}{},
+		parseFile: func(file string, val interface{}) error {
+			v, ok := val.(map[string]interface{})
+			if !ok {
+				panic("assert failed")
+			}
+			v[key] = file
+			return nil
+		},
 	}
-	if !reflect.DeepEqual(reader.data, fakeData) {
-		test.Errorf("deserialized content was incorrect, got: %s, want: %s.", reader.data, fakeData)
+	expected := "old value"
+	s.data[key] = expected
+	s.loadPath("new value")
+
+	if actual, ok := s.data[key]; ok {
+		if actual != "new value" {
+			test.Fatalf("%+v was expected, got %+v", expected, actual)
+		}
+	} else {
+		test.Fatalf("key %+v not found", key)
 	}
 }
 
-func TestFileNameForConfig(test *testing.T) {
-	reader := ConfigFileReader{
-		Basedir:  "/etc/paasta",
-		Filename: "volumes.json",
+func TestStore_loadAll(test *testing.T) {
+	s := &Store{
+		dir:  "zero",
+		data: map[string]interface{}{},
+		parseFile: func(file string, val interface{}) error {
+			fmt.Printf("parse file called: %s\n", file)
+			v, ok := val.(map[string]interface{})
+			if !ok {
+				panic("assert failed")
+			}
+			v[file] = "loaded"
+			return nil
+		},
+		listFiles: func(dirname string) ([]string, error) {
+			fmt.Println("list files called")
+			return []string{"one", "two"}, nil
+		},
 	}
-	expected := "/etc/paasta/volumes.json"
-	actual := reader.FileNameForConfig()
-	if actual != expected {
-		test.Errorf("filename incorrect incorrect, got: %s, want: %s.", actual, expected)
+	s.loadAll()
+
+	for _, key := range []string{"zero/one", "zero/two"} {
+		if actual, ok := s.data[key]; ok {
+			if actual != "loaded" {
+				test.Fatalf("%s wasn't loaded correctly", key)
+			}
+		} else {
+			test.Fatalf("key %+v wasn't loaded", key)
+		}
 	}
+}
+
+func TestStore_load(test *testing.T) {
+	s := &Store{
+		dir:  "zero",
+		data: map[string]interface{}{},
+		parseFile: func(file string, val interface{}) error {
+			return nil
+		},
+		listFiles: func(dirname string) ([]string, error) {
+			test.Fatalf("unexpected call to listFiles(%s)", dirname)
+			return []string{}, nil
+		},
+		fileExists: func(path string) (bool, error) {
+			expected := "zero/one.json"
+			if path != expected {
+				test.Fatalf("expected path=%s, got path=%s", expected, path)
+			}
+			return true, nil
+		},
+	}
+	s.load("one")
+
+	s.fileExists = func(path string) (bool, error) {
+		return false, nil
+	}
+	s.parseFile = func(file string, val interface{}) error {
+		test.Fatalf("unexpected call to parseFile(%s)", file)
+		return nil
+	}
+	s.listFiles = func(dirname string) ([]string, error) {
+		return []string{}, nil
+	}
+	s.load("one")
+}
+
+func TestStore_Get(test *testing.T) {
+	s := &Store{
+		dir:  "zero",
+		data: map[string]interface{}{},
+		parseFile: func(file string, val interface{}) error {
+			test.Fatalf("unexpected call to parseFile(%s)", file)
+			return nil
+		},
+		listFiles: func(dirname string) ([]string, error) {
+			test.Fatalf("unexpected call to listFiles(%s)", dirname)
+			return []string{}, nil
+		},
+		fileExists: func(path string) (bool, error) {
+			test.Fatalf("unexpected call to fileExists(%s)", path)
+			return true, nil
+		},
+	}
+	s.data["one"] = "two"
+	val, ok := s.Get("one")
+	errorIf(test, !ok, "key not found")
+	errorUnexpected(test, "two", val)
+
+	// key is missing, file with same name exists
+	s.fileExists = func(path string) (bool, error) { return true, nil }
+	s.parseFile = func(file string, val interface{}) error {
+		v := val.(map[string]interface{})
+		v["two"] = "three"
+		return nil
+	}
+	val, ok = s.Get("two")
+	errorIf(test, !ok, "key not found")
+	errorUnexpected(test, "three", val)
+
+	// key is missing, file corresponding to a hint exists
+	s.hints = map[string]string{"three": "four"}
+	s.fileExists = func(path string) (bool, error) {
+		errorUnexpected(test, "zero/four.json", path)
+		return true, nil
+	}
+	s.parseFile = func(file string, val interface{}) error {
+		errorUnexpected(test, "zero/four.json", file)
+		v := val.(map[string]interface{})
+		v["three"] = "four"
+		return nil
+	}
+	val, ok = s.Get("three")
+	errorIf(test, !ok, "key not found")
+	errorUnexpected(test, "four", val)
+
+	// key is missing, file is missing, hint is missing, loaded from all
+	val, ok = s.Get("four")
+	errorIf(test, !ok, "key not found")
+	errorUnexpected(test, "five", val)
+
+	// hint is wrong
+
+	// missing from everywhere
 }
