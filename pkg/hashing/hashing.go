a689c520572cb71739f155ddbb6dde3d57e47daf
Ashish Khatkar <akhatkar64@gmail.com>
2020-03-02 15:43:24 +0000

    Adding support to get Yaml for k8s object (#45)
    * Adding support to get Yaml for k8s object
    
    * Added logic to compute yaml diff, refactored hashing helper
    
    * Nit changes for review
    
    * Updating link for unified context

--- pkg/hashing/hashing.go
+++ pkg/hashing/hashing.go
@@ -9,44 +9,17 @@ import (
 )
 
 func ComputeHashForKubernetesObject(object interface{}) (string, error) {
-	// By marshaling/unmarshaling the object via JSON we're copying it into a
-	// map, which is easier to manipulate in generic way than structs.
-	if b, err := json.Marshal(object); err != nil {
-		return "", fmt.Errorf("Error while encoding %+v into JSON: %s", object, err)
+	if m, err := GetFilteredK8sObjectForHashing(object); err != nil {
+		return "", err
 	} else {
-		var v map[string]interface{}
-		if err := json.Unmarshal(b, &v); err != nil {
-			return "", fmt.Errorf("Error while decoding JSON %s into an object: %s", v, err)
+		// By using serialized JSON for hashing we're making the hashing process
+		// a bit easier (like having maps always being sorted by keys).
+		if b, err := json.Marshal(m); err != nil {
+			return "", fmt.Errorf("error while encoding %+v into JSON: %s", m, err)
 		} else {
-			// We need only kind/version/spec and labels excluding the label with the
-			// current hash value while calculating the hash.  Also Kubernetes adds
-			// its own info into `metadata` which we need to ignore.
-			meta := v["metadata"].(map[string]interface{})
-			labels := meta["labels"]
-			if labels != nil {
-				delete(labels.(map[string]interface{}), "yelp.com/operator_config_hash")
-			} else {
-				labels = make(map[string]interface{})
-			}
-			m := map[string]interface{}{
-				"kind":       v["kind"],
-				"apiVersion": v["apiVersion"],
-				"spec":       v["spec"],
-				"metadata": map[string]interface{}{
-					"name":      meta["name"],
-					"namespace": meta["namespace"],
-					"labels":    labels,
-				},
-			}
-			// By using serialized JSON for hashing we're making the hashing process
-			// a bit easier (like having maps always being sorted by keys).
-			if b, err := json.Marshal(m); err != nil {
-				return "", fmt.Errorf("Error while encoding %+v into JSON: %s", m, err)
-			} else {
-				hasher := fnv.New32a()
-				hasher.Write(b)
-				return rand.SafeEncodeString(fmt.Sprint(hasher.Sum32())), nil
-			}
+			hasher := fnv.New32a()
+			hasher.Write(b)
+			return rand.SafeEncodeString(fmt.Sprint(hasher.Sum32())), nil
 		}
 	}
 }
@@ -58,7 +31,7 @@ func SetKubernetesObjectHash(configHash string, object interface{}) error {
 	if value.Kind() == reflect.Ptr {
 		objectMeta = value.Elem().FieldByName("ObjectMeta")
 	} else {
-		return fmt.Errorf("Must pass pointer to AddLabelsToMetadata so we can update labels using reflection.")
+		return fmt.Errorf("must pass pointer to AddLabelsToMetadata so we can update labels using reflection")
 	}
 	if objectMeta.Kind() == reflect.Struct {
 		labels := objectMeta.FieldByName("Labels")
---
+++ pkg/utils/yamlhelper.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/pmezard/go-difflib/difflib"
+	"gopkg.in/yaml.v2"
+)
+
+// function to get the yaml output of an object
+// marshal/unmarshal to and from json in order to maintain consistency between json and yaml
+// currently k8s objects have json field tags, so this helps in skipping empty fields
+// and give consistent output yaml similar to json
+func getYamlOfObject(object interface{}) (string, error) {
+	if b, err := json.Marshal(object); err != nil {
+		return "", fmt.Errorf("error while encoding %+v into JSON: %s", object, err)
+	} else {
+		var v map[string]interface{}
+		if err := json.Unmarshal(b, &v); err != nil {
+			return "", fmt.Errorf("error while decoding JSON %s into an object: %s", v, err)
+		} else {
+			if y, err := yaml.Marshal(&v); err != nil {
+				return "", fmt.Errorf("error while encoding Map %s into YAML: %s", v, err)
+			} else {
+				return string(y), nil
+			}
+		}
+	}
+}
+
+func generateYamlDiff(yaml1 string, yaml2 string, context int) string {
+	diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+		A:        difflib.SplitLines(yaml1),
+		B:        difflib.SplitLines(yaml2),
+		FromFile: "Old",
+		FromDate: "",
+		ToFile:   "New",
+		ToDate:   "",
+		Context:  context,
+	})
+	return diff
+}
+
+// func to generate yaml diff of objects used for hashing
+// context : number of context lines to use for generating diff
+// for more reference on context : https://www.gnu.org/software/diffutils/manual/html_node/Unified-Format.html#Unified-Format
+func GetYamlDiffForObjects(objectOld interface{}, objectNew interface{}, context int) (string, error) {
+	yamlOld, err := getYamlOfObject(objectOld)
+	if err != nil {
+		return "", err
+	}
+	yamlNew, err := getYamlOfObject(objectNew)
+	if err != nil {
+		return "", err
+	}
+	return generateYamlDiff(yamlOld, yamlNew, context), nil
+}
