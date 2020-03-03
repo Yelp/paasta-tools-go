d262936c7e50f95305e10fdc3cf33ee74701d11b
Bronek Kozicki <bronek@yelp.com>
2020-03-03 12:37:33 +0000

    Refactoring of WaitFor to use int rather than *int32
    This is breaking change, but getting over if will actually simplify majority of uses of WaitFor. The intent of the original design was to allow the users to directly pass e.g. deployment.Spec.Replicas which is *int32, but this turned out not be be very useful, while at the same time creating friction for simpler case of just expecting a fixed number of pods to show up. The original design has also spawned the helper function WaitForNone which is no longer needed, because the user can just as well pass 0 to WaitFor.

--- pkg/test_framework/helpers.go
+++ pkg/test_framework/helpers.go
@@ -152,28 +152,14 @@ func LoadInto(r io.Reader, into interface{}) error {
 
 type WaitForFn func()(interface{}, error)
 
-// Wait until WaitSource returns 0 elements or NotFound error - useful for waiting until
-// something is deleted
-func WaitForNone(timeout time.Duration, from WaitForFn) error {
-	none := int32(0)
-	return WaitFor(&none, timeout, from)
-}
-
 // Wait until from returns a given number of instances. In the context of strongly
 // typed non-List k8s objects this typically means number of ready pods; for List objects
 // (including UnstructuredList) this means number of items; for Unstructured object
 // this means hardcoded 1. For details, refer to getReady function.
-// We use a pointer int32 to allow the use of ...Spec.Replicas; for the same reason we
-// fallback to hardcoded 1 if nil is provided
-func WaitFor(reps* int32, timeout time.Duration, from WaitForFn) error {
-	wanted := int32(1)
-	if reps != nil {
-		wanted = *reps
-	}
-
+func WaitFor(wanted int, timeout time.Duration, from WaitForFn) error {
 	return wait.Poll(time.Second, timeout, func() (bool, error) {
 		current, err := from()
-		ready := int32(0)
+		ready := 0
 		if err != nil {
 			if !errors.IsNotFound(err) {
 				return false, err
@@ -192,64 +178,64 @@ func WaitFor(reps* int32, timeout time.Duration, from WaitForFn) error {
 	})
 }
 
-func getReady(obj interface{}) (int32, error) {
+func getReady(obj interface{}) (int, error) {
 	switch t := obj.(type) {
 	case *appsv1.StatefulSet:
-		return (*t).Status.ReadyReplicas, nil
+		return int((*t).Status.ReadyReplicas), nil
 	case *appsv1.Deployment:
-		return (*t).Status.ReadyReplicas, nil
+		return int((*t).Status.ReadyReplicas), nil
 	case *appsv1.DaemonSet:
-		return (*t).Status.NumberReady, nil
+		return int((*t).Status.NumberReady), nil
 	case *appsv1.ReplicaSet:
-		return (*t).Status.ReadyReplicas, nil
+		return int((*t).Status.ReadyReplicas), nil
 	case *batchv1.Job:
-		return (*t).Status.Active, nil
+		return int((*t).Status.Active), nil
 	case *appsv1.StatefulSetList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *appsv1.DeploymentList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *appsv1.DaemonSetList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *appsv1.ReplicaSetList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *appsv1.ControllerRevisionList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *batchv1.JobList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.PersistentVolumeList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.PersistentVolumeClaimList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.PodList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.ServiceList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.ServiceAccountList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.EndpointsList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.NodeList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.NamespaceList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.EventList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.SecretList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.ConfigMapList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *corev1.ComponentStatusList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *rbacv1.RoleBindingList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *rbacv1.RoleList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *rbacv1.ClusterRoleBindingList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *rbacv1.ClusterRoleList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *unstructured.UnstructuredList:
-		return int32(len((*t).Items)), nil
+		return len((*t).Items), nil
 	case *unstructured.Unstructured:
 		if !t.IsList() {
 			// Consider single object an equivalent for a list of 1
@@ -259,13 +245,13 @@ func getReady(obj interface{}) (int32, error) {
 		if err != nil {
 			log.Panic(err)
 		}
-		return int32(len(list.Items)), nil
+		return len(list.Items), nil
 	case int64:
-		return int32(t), nil
+		return int(t), nil
 	case int32:
-		return t, nil
+		return int(t), nil
 	case int:
-		return int32(t),nil
+		return t, nil
 	default:
 		log.Panicf("Unsupported type %t", obj)
 	}
