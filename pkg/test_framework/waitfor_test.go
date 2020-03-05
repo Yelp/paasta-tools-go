de6bce06bc30ebbc0da1732076102556576db27c
Bronek Kozicki <bronek@yelp.com>
2020-03-05 19:11:42 +0000

    Test WaitFor with actual Kubernetes cluster
    Requires k3d and docker, I only expect this to work on Linux

---
+++ pkg/test_framework/waitfor_test.go
@@ -0,0 +1,38 @@
+package framework
+
+import (
+	"context"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func TestWaitFor_Basic(t *testing.T) {
+	if runtime.GOOS != "linux" {
+		t.Skip("This test is meant to run on Linux only")
+		return
+	}
+	options := *newOptions("itest")
+	Start(nil, options, Sinks{})
+	defer Kube.Close()
+
+	// See if the kubernetes service is available
+	err := WaitFor(
+		1,
+		time.Minute,
+		func()(interface{}, error) {
+			res := &corev1.ServiceList{}
+			err := Kube.client.List(
+				context.TODO(),
+				&client.ListOptions{Namespace: "default"},
+				res,
+			)
+			return res, err
+		},
+	)
+	assert.NoError(t, err)
+}
